717b7aa2d4aa60e023957c713d300b6b0073e764
Agustin <agu_deoro@hotmail.com>
2022-03-22 22:35:27 -0300

    homework JSII

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -189,7 +189,6 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return ('nombre +  apellido');
   
 }
 
@@ -197,7 +196,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return('hola' + nombre + '!');
+  return('Hola' + nombre + '!');
   
 }
 
@@ -240,10 +239,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {return 'es vocal'};
-  if(letra.length > 1) {return 'dato incorrecto';}
-  else {return 'dato incorrecto'};
-
+  if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {return 'Es vocal'};
+  if(letra.length > 1) {return 'Dato incorrecto'};
+  return 'Dato incorrecto';
 
 }
 
